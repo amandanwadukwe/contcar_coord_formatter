2c1afa04a5eb4432f9b5249670d802c362dfba05
amandanwadukwe <76092145+amandanwadukwe@users.noreply.github.com>
2022-09-30 14:56:22 +0100

    Updated code to format multiple files at once

--- newCoordFormatter.py
+++ newCoordFormatter.py
@@ -2,70 +2,73 @@ import csv
 import sys
 
 #Make each line of in a coords txt file into an item  in an array
-lines = []
-with open(sys.argv[1]) as coords:
-    while True:
-        line = coords.readline()
-        if not line:
-            break
-        lines.append(line.strip())
-
 def filter_line(line_string, cond_num):
-    arrayed_and_filtered = []
-    for occurence in line_string.split(" "):
-        if len(occurence) > cond_num:
-            arrayed_and_filtered.append(occurence)
-    return arrayed_and_filtered
-
-v1 = filter_line(lines[2], 0)
-v2 = filter_line(lines[3], 0)
-v3 = filter_line(lines[4], 0)
-elements = []
-number_of_atoms = []
-el_and_atoms_list = []
-
-#Extract the number of atoms of each element
-number_of_atoms = filter_line(lines[6], 0)
-
-#Extract all the elements into the array called "elements"
-elements = filter_line(lines[5], 0)
-
-for index in range(len(number_of_atoms)):
-    el_and_atoms_list.append(elements[index])
-    el_and_atoms_list.append(int(number_of_atoms[index]))
-
-item = iter(el_and_atoms_list)
-el_and_atoms_dict = dict(zip(item, item))
+            arrayed_and_filtered = []
+            for occurence in line_string.split(" "):
+                if len(occurence) > cond_num:
+                    arrayed_and_filtered.append(occurence)
+            return arrayed_and_filtered
 
 def calculate_position_of_each_atom(el, start_point, end_point):
-    positions = []
-    
-    for index in range(len(lines)):
-        if index <= end_point and index > start_point:
-            x_pos = float(filter_line(lines[index], 1)[0]) * float(v1[0]) + float(filter_line(lines[index], 1)[1]) * float(v2[0]) + float(filter_line(lines[index], 1)[2]) * float(v3[0])
-            y_pos = float(filter_line(lines[index], 1)[0]) * float(v1[1]) + float(filter_line(lines[index], 1)[1]) * float(v2[1]) + float(filter_line(lines[index], 1)[2]) * float(v3[1])
-            z_pos = float(filter_line(lines[index], 1)[0]) * float(v1[2]) + float(filter_line(lines[index], 1)[1]) * float(v2[2]) + float(filter_line(lines[index], 1)[2]) * float(v3[2])
+            positions = []
+            
+            for index in range(len(lines)):
+                if index <= end_point and index > start_point:
+                    x_pos = float(filter_line(lines[index], 1)[0]) * float(v1[0]) + float(filter_line(lines[index], 1)[1]) * float(v2[0]) + float(filter_line(lines[index], 1)[2]) * float(v3[0])
+                    y_pos = float(filter_line(lines[index], 1)[0]) * float(v1[1]) + float(filter_line(lines[index], 1)[1]) * float(v2[1]) + float(filter_line(lines[index], 1)[2]) * float(v3[1])
+                    z_pos = float(filter_line(lines[index], 1)[0]) * float(v1[2]) + float(filter_line(lines[index], 1)[1]) * float(v2[2]) + float(filter_line(lines[index], 1)[2]) * float(v3[2])
 
-            positions.append([x_pos, y_pos, z_pos])
-    
-    return positions
-                
-with open('newCoordFormat.xyz', 'w', encoding='UTF8') as newCoordFormat:
-    writer = csv.writer(newCoordFormat, delimiter=" ")
+                    positions.append([x_pos, y_pos, z_pos])
+            
+            return positions
 
-    header = [77]
-    writer.writerow(header)
+for file_index in range(len(sys.argv)):
+    if( file_index > 0):
+        lines = []
+        with open(sys.argv[file_index]) as coords:
+            while True:
+                line = coords.readline()
+                if not line:
+                    break
+                lines.append(line.strip())
+
+        v1 = filter_line(lines[2], 0)
+        v2 = filter_line(lines[3], 0)
+        v3 = filter_line(lines[4], 0)
+        elements = []
+        number_of_atoms = []
+        el_and_atoms_list = []
+
+        #Extract the number of atoms of each element
+        number_of_atoms = filter_line(lines[6], 0)
+
+        #Extract all the elements into the array called "elements"
+        elements = filter_line(lines[5], 0)
 
 
-    # write the data
-    for data in calculate_position_of_each_atom("C",77,121):
-        writer.writerow(data)
+        for index in range(len(number_of_atoms)):
+            el_and_atoms_list.append(elements[index])
+            el_and_atoms_list.append(int(number_of_atoms[index]))
 
-    for data in calculate_position_of_each_atom("H",121, 149):
-        writer.writerow(data)
+        item = iter(el_and_atoms_list)
+        el_and_atoms_dict = dict(zip(item, item))    
+                        
+        with open(sys.argv[file_index][slice(0, -4)] + ".xyz", 'w', encoding='UTF8') as newCoordFormat:
+            writer = csv.writer(newCoordFormat, delimiter=" ")
 
-    for data in calculate_position_of_each_atom("N", 149, 153):
-        writer.writerow(data)
+            header = [77]
+            writer.writerow(header)
 
-    for data in calculate_position_of_each_atom("Zn", 153, 154):
-        writer.writerow(data)
+
+            # write the data
+            for data in calculate_position_of_each_atom("C",77,121):
+                writer.writerow(data)
+
+            for data in calculate_position_of_each_atom("H",121, 149):
+                writer.writerow(data)
+
+            for data in calculate_position_of_each_atom("N", 149, 153):
+                writer.writerow(data)
+
+            for data in calculate_position_of_each_atom("Zn", 153, 154):
+                writer.writerow(data)

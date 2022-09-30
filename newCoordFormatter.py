17b3a8ad02b96275e8272951f9c5e8ccf03bfca5
amandanwadukwe <76092145+amandanwadukwe@users.noreply.github.com>
2022-09-30 15:53:11 +0100

    Update newCoordFormatter.py
    softcoded writing to files for each file to run through the pipeline

--- newCoordFormatter.py
+++ newCoordFormatter.py
@@ -1,7 +1,6 @@
 import csv  
 import sys
 
-#Make each line of in a coords txt file into an item  in an array
 def filter_line(line_string, cond_num):
             arrayed_and_filtered = []
             for occurence in line_string.split(" "):
@@ -9,11 +8,11 @@ def filter_line(line_string, cond_num):
                     arrayed_and_filtered.append(occurence)
             return arrayed_and_filtered
 
-def calculate_position_of_each_atom(el, start_point, end_point):
+def calculate_position_of_each_atom(array_of_atom_details):
             positions = []
-            
+            #  where the endpoint = array_of_atom_details[2], startpoint = array_of_atom_details[1] and array_of_atom_details[0] is just a label
             for index in range(len(lines)):
-                if index <= end_point and index > start_point:
+                if index <= array_of_atom_details[2] and index > array_of_atom_details[1]:
                     x_pos = float(filter_line(lines[index], 1)[0]) * float(v1[0]) + float(filter_line(lines[index], 1)[1]) * float(v2[0]) + float(filter_line(lines[index], 1)[2]) * float(v3[0])
                     y_pos = float(filter_line(lines[index], 1)[0]) * float(v1[1]) + float(filter_line(lines[index], 1)[1]) * float(v2[1]) + float(filter_line(lines[index], 1)[2]) * float(v3[1])
                     z_pos = float(filter_line(lines[index], 1)[0]) * float(v1[2]) + float(filter_line(lines[index], 1)[1]) * float(v2[2]) + float(filter_line(lines[index], 1)[2]) * float(v3[2])
@@ -22,8 +21,19 @@ def calculate_position_of_each_atom(el, start_point, end_point):
             
             return positions
 
+def get_all_output(array_of_elment_start_and_end_points):
+    all_data = []
+    for data in array_of_elment_start_and_end_points:
+        for positions in calculate_position_of_each_atom(data):
+            all_data.append(positions)
+
+    return all_data
+
 for file_index in range(len(sys.argv)):
+    #For each file
     if( file_index > 0):
+        file_detials = [["C",77,121], ["H",121, 149],["N", 149, 153],["Zn", 153, 154]]
+        #Make each line of in a coords txt file into an item  in an array
         lines = []
         with open(sys.argv[file_index]) as coords:
             while True:
@@ -35,40 +45,15 @@ for file_index in range(len(sys.argv)):
         v1 = filter_line(lines[2], 0)
         v2 = filter_line(lines[3], 0)
         v3 = filter_line(lines[4], 0)
-        elements = []
-        number_of_atoms = []
-        el_and_atoms_list = []
-
-        #Extract the number of atoms of each element
-        number_of_atoms = filter_line(lines[6], 0)
-
-        #Extract all the elements into the array called "elements"
-        elements = filter_line(lines[5], 0)
-
-
-        for index in range(len(number_of_atoms)):
-            el_and_atoms_list.append(elements[index])
-            el_and_atoms_list.append(int(number_of_atoms[index]))
-
-        item = iter(el_and_atoms_list)
-        el_and_atoms_dict = dict(zip(item, item))    
                         
+        #Create a file
         with open(sys.argv[file_index][slice(0, -4)] + ".xyz", 'w', encoding='UTF8') as newCoordFormat:
             writer = csv.writer(newCoordFormat, delimiter=" ")
 
-            header = [77]
+            # write the data
+            header = []
+            header.append(len(get_all_output(file_detials)))
             writer.writerow(header)
 
-
-            # write the data
-            for data in calculate_position_of_each_atom("C",77,121):
-                writer.writerow(data)
-
-            for data in calculate_position_of_each_atom("H",121, 149):
-                writer.writerow(data)
-
-            for data in calculate_position_of_each_atom("N", 149, 153):
-                writer.writerow(data)
-
-            for data in calculate_position_of_each_atom("Zn", 153, 154):
-                writer.writerow(data)
+            for output in get_all_output(file_detials):
+                writer.writerow(output)
